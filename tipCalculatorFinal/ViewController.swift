af7c93b3b3c339d574919f8b176e46a1179abec9
Rinchen Lama <rinchenlama0075@gmail.com>
2020-10-04 17:13:07 -0500

    Initial Commit

---
+++ tipCalculatorFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculatorFinal
+//
+//  Created by Rinchen on 10/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
