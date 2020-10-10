9338d6fa4e9593de395e0f994d4b004ae67465c4
Rinchen Lama <rinchenlama0075@gmail.com>
2020-10-10 15:03:17 -0500

    added appIcon and minor tweaks

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ Time spent: **12** hours spent in total
 
 The following **required** functionality is complete:
 
-* [ ] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
-* [ ] User can select between tip percentages by tapping different values on the segmented control and the tip value is updated accordingly
+* [ X] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
+* [ X] User can select between tip percentages by tapping different values on the segmented control and the tip value is updated accordingly
 
 The following **optional** features are implemented:
 
 * [ ] UI animations
 * [ ] Remembering the bill amount across app restarts (if <10mins)
 * [ ] Using locale-specific currency and currency thousands separators.
-* [ ] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+* [ X] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
 
 The following **additional** features are implemented:
 
@@ -35,6 +35,7 @@ GIF created with [LiceCap](http://www.cockos.com/licecap/).
 ## Notes
 
 Describe any challenges encountered while building the app.
+I was trying to learn and explore Swift more as I hadn't really used it before. The drag-and-link type 
 
 ## License
 
--- tipCalculatorFinal/ViewController.swift
+++ tipCalculatorFinal/ViewController.swift
@@ -7,6 +7,38 @@
 
 import UIKit
 
+//Formatting number for local currency
+
+extension Formatter {
+    static let number = NumberFormatter()
+}
+extension Locale {
+    static let englishUS: Locale = .init(identifier: "en_US")
+    static let frenchFR: Locale = .init(identifier: "fr_FR")
+    static let portugueseBR: Locale = .init(identifier: "pt_BR")
+    // ... and so on
+}
+extension Numeric {
+    func formatted(with groupingSeparator: String? = nil, style: NumberFormatter.Style, locale: Locale = .current) -> String {
+        Formatter.number.locale = locale
+        Formatter.number.numberStyle = style
+        if let groupingSeparator = groupingSeparator {
+            Formatter.number.groupingSeparator = groupingSeparator
+        }
+        return Formatter.number.string(for: self) ?? ""
+    }
+    // Localized
+    var currency:   String { formatted(style: .currency) }
+    // Fixed locales
+    var currencyUS: String { formatted(style: .currency, locale: .englishUS) }
+    var currencyFR: String { formatted(style: .currency, locale: .frenchFR) }
+    var currencyBR: String { formatted(style: .currency, locale: .portugueseBR) }
+    // ... and so on
+//    var calculator: String { formatted(groupingSeparator: style: .decimal) }
+}
+
+//end currency format funcitonality
+
 class ViewController: UIViewController {
     @IBOutlet weak var TotalLabel: UILabel!
     @IBOutlet weak var SuggestedTipLabel: UILabel!
@@ -17,8 +49,11 @@ class ViewController: UIViewController {
     var billAmount = 0.00;
     var tipPercentages = [10, 20, 30];
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        BillAmountLabel.becomeFirstResponder()
         // Do any additional setup after loading the view.
 //        foodImage.image = UIImage(named: "background")
     }
@@ -30,15 +65,15 @@ class ViewController: UIViewController {
     @IBAction func BillAmountChange(_ sender: Any) {
         billAmount = Double(BillAmountLabel.text!) ?? 0
 //        var suggestedTip = billAmount*0.18
-        SuggestedTipLabel.text = String(format: "$%.2f",billAmount*0.18)
-        TotalLabel.text = String(format: "$%.2f", billAmount*0.18 + billAmount)
-        CustomTipAmountLabel.text = String( format: "$%.2f", billAmount * Double(tipPercentages[SegmentedTipSelect.selectedSegmentIndex])/100 ) }
+        SuggestedTipLabel.text = String(format: "%.2f",(billAmount*0.18))
+        TotalLabel.text = String(format: "%.2f", billAmount*0.18 + billAmount)
+        CustomTipAmountLabel.text = String( format: "%.2f", (billAmount * Double(tipPercentages[SegmentedTipSelect.selectedSegmentIndex])/100).currencyUS ) }
     
     
     @IBAction func SegmentedTipValueChanged(_ sender: Any) {
         billAmount = Double(BillAmountLabel.text!) ?? 0
-        CustomTipAmountLabel.text = String(format: "$%.2f",billAmount * Double(tipPercentages[SegmentedTipSelect.selectedSegmentIndex])/100 )
-        TotalLabel.text = String(format: "$%.2f", billAmount * Double(tipPercentages[SegmentedTipSelect.selectedSegmentIndex])/100 + billAmount)
+        CustomTipAmountLabel.text = String(format: "%.2f",billAmount * Double(tipPercentages[SegmentedTipSelect.selectedSegmentIndex])/100 )
+        TotalLabel.text = String(format: "%.2f", (billAmount * Double(tipPercentages[SegmentedTipSelect.selectedSegmentIndex])/100 + billAmount).currencyUS)
         
     }
     
