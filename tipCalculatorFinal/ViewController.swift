8fa2fd82c51376ace4596518bf3b833746148430
Rinchen Lama <rinchenlama0075@gmail.com>
2020-10-10 15:25:04 -0500

    removed unfinished modules

--- tipCalculatorFinal/ViewController.swift
+++ tipCalculatorFinal/ViewController.swift
@@ -7,37 +7,6 @@
 
 import UIKit
 
-//Formatting number for local currency
-
-extension Formatter {
-    static let number = NumberFormatter()
-}
-extension Locale {
-    static let englishUS: Locale = .init(identifier: "en_US")
-    static let frenchFR: Locale = .init(identifier: "fr_FR")
-    static let portugueseBR: Locale = .init(identifier: "pt_BR")
-    // ... and so on
-}
-extension Numeric {
-    func formatted(with groupingSeparator: String? = nil, style: NumberFormatter.Style, locale: Locale = .current) -> String {
-        Formatter.number.locale = locale
-        Formatter.number.numberStyle = style
-        if let groupingSeparator = groupingSeparator {
-            Formatter.number.groupingSeparator = groupingSeparator
-        }
-        return Formatter.number.string(for: self) ?? ""
-    }
-    // Localized
-    var currency:   String { formatted(style: .currency) }
-    // Fixed locales
-    var currencyUS: String { formatted(style: .currency, locale: .englishUS) }
-    var currencyFR: String { formatted(style: .currency, locale: .frenchFR) }
-    var currencyBR: String { formatted(style: .currency, locale: .portugueseBR) }
-    // ... and so on
-//    var calculator: String { formatted(groupingSeparator: style: .decimal) }
-}
-
-//end currency format funcitonality
 
 class ViewController: UIViewController {
     @IBOutlet weak var TotalLabel: UILabel!
